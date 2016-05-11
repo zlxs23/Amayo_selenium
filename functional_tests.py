14bb7fc3d27decc3264c5c86fa3acc65488c544b
zlxs23 <zilanxingshuo@gmail.com>
2016-05-12 00:02:20 +0800

    alter functional_tests add class

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,22 @@
 from selenium import webdriver
-brower = webdriver.Firefox()
-brower.get('http://localhost:8000')
-assert 'Django' in brower.title
+import unittest
+
+
+class NewVisitorTest(unittest.TestCase):
+
+    """docstring for NewVisitorTest"""
+
+    def setUp(self):
+        self.brower = webdriver.Firefox()
+        self.brower.implicitly_wait(3)	# selenium 常用方法
+
+    def tearDown(self):
+        self.brower.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.brower.get('http://localhost:8000')
+        self.assertIn('To-Do', self.brower.title)
+        self.fail('Finish the Test!')
+
+if __name__ == '__main__':
+    unittest.main()
